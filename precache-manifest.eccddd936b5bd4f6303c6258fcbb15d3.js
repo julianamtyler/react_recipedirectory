5ae252d1060e50fe800b2fda8d16f52059b06f51
julianamtyler <julianam.tyler@gmail.com>
2018-12-30 11:48:34 -0500

    Updates

---
+++ precache-manifest.eccddd936b5bd4f6303c6258fcbb15d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa335daf7a898fe43165",
+    "url": "/react_recipedirectory/static/js/runtime~main.aa335daf.js"
+  },
+  {
+    "revision": "7e7a6d9e759f74871bad",
+    "url": "/react_recipedirectory/static/js/main.7e7a6d9e.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/react_recipedirectory/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "ca4674bc0b549a5e0e5e2fff9dcdb113",
+    "url": "/react_recipedirectory/index.html"
+  }
+];
