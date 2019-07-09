9d22d4110cf3d75dcb1bb4da8af87ba5e65f0515
julianamtyler <julianam.tyler@gmail.com>
2019-07-08 21:31:20 -0400

    Updates

--- precache-manifest.3ca310940f2f9dc27be6a49d6a2bd541.js
+++ precache-manifest.3ca310940f2f9dc27be6a49d6a2bd541.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react_recipedirectory/static/js/runtime~main.aa335daf.js"
   },
   {
-    "revision": "86b8a09183076167433a",
-    "url": "/react_recipedirectory/static/js/main.86b8a091.chunk.js"
+    "revision": "58e635ecc097b1f4dd08",
+    "url": "/react_recipedirectory/static/js/main.58e635ec.chunk.js"
   },
   {
     "revision": "a44f0c0078e70b51841d",
     "url": "/react_recipedirectory/static/js/1.a44f0c00.chunk.js"
   },
   {
-    "revision": "86b8a09183076167433a",
-    "url": "/react_recipedirectory/static/css/main.d90a0cdd.chunk.css"
+    "revision": "58e635ecc097b1f4dd08",
+    "url": "/react_recipedirectory/static/css/main.f8d57fc0.chunk.css"
   },
   {
-    "revision": "39172f2a6604e603a8f7d76d3da895aa",
+    "revision": "9e71b09982f29566885bc2b11d4530ed",
     "url": "/react_recipedirectory/index.html"
   }
 ];
